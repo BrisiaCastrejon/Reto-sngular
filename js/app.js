e880c6068daa39fced536a75671d9892fd020b26
BrisiaCastrejon <brissmonse@gmail.com>
2018-11-27 14:42:46 -0600

    Cambio para prueba

--- js/app.js
+++ js/app.js
@@ -3,9 +3,10 @@ const newText = document.querySelector('input');
 const titleButton = document.querySelector('a');
 
 titleButton.addEventListener('click', (event) => {
-
+  console.log("prueba de gitHubPages");
+  
   theTitle.innerHTML = newText.value;
   theTitle.classList.add('estiloNuevo');
   newText.value = '';  
-  
+
 })
