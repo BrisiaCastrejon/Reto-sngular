54a63a9c6cf8efc6c8cbc47ed433163c3df51f21
BrisiaCastrejon <brissmonse@gmail.com>
2018-11-27 13:53:47 -0600

    Cambia el título con nuevos estilos

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const theTitle = document.querySelector('h1');
+const newText = document.querySelector('input');
+const titleButton = document.querySelector('a');
+
+titleButton.addEventListener('click', (event) => {
+  theTitle.innerHTML = newText.value;
+  theTitle.classList.add('estiloNuevo')
+  newText.value = '';  
+})
